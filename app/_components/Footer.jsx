5fb4c2bdbc544344e396bae17b2a67e7bdf2c19b
Demisquier <dema2910@gmail.com>
2026-08-20 15:09:11 -0300

    Update Footer.jsx

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -48,6 +48,7 @@ export default function Footer() {
             [CONTACT_EMAIL, `mailto:${CONTACT_EMAIL}`],
             ["Guías y novedades", "/novedades/"],
             ["Nosotros", "/sobre-nosotros/"],
+            ["Créditos y fuentes", "/creditos-y-fuentes/"],
           ]} />
         </div>
 
